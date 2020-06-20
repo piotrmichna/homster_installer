9d96db1a568f1b8c01569788205b18c892d44c25
Piotr Michna <pm@piotrmichna.pl>
2020-06-21 01:08:40 +0200

    definicja funkcji instalacji oprogramowania install_prog()

--- lib_install.sh
+++ lib_install.sh
@@ -4,3 +4,14 @@
 #   instalator i konfigurator systemu HOMSTER
 #   autor: Piotr Michna
 #   email: pm@piotrmichna.pl
+
+function install_prog(){
+    local mess=""
+    dpkg -s $1 &> /dev/null
+    if [ $? -eq 0 ] ; then
+        TERM=ansi whiptail --title "- $1 -" --infobox "$1 był już zainstalowany" 8 70
+    else
+        sudo apt-get install -y $1 &> /dev/null
+        TERM=ansi whiptail --title "INSTALATOR - $1" --infobox "Oprogramowanie $1 zostało pomyślnie zainstalowane." 8 70
+    fi
+}
