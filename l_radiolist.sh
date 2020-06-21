8f1d3b0a6af3eb2bd5c67f6088de7bd5635403d2
Piotr Michna <pm@piotrmichna.pl>
2020-06-21 17:16:20 +0200

    definicja funkcji radiolist_var()

--- l_radiolist.sh
+++ l_radiolist.sh
@@ -45,3 +45,59 @@ menux[9]="4)"
 menux[10]="Koniec."
 menux[11]="off"
 
+function radiolist_var(){
+    local tyt="Tytuł"
+    local message="Piotr Michna\npm@piotrmichna.pl\n"
+    local width=78
+    local nline=${#menux[@]}
+    nline=$(( nline/3 ))
+    local height=$(( nline+8 ))
+
+    for (( i=1 ; i<=${#@} ; i++ )) ; do
+        tt=$(echo "\$$i" )
+        tvar=$(eval echo "$tt")
+
+        if [ $tvar = "-t" ] ; then
+            i=$(( i+1 ))
+            tt=$(echo "\$$i" )
+            tvar=$(eval echo "$tt")
+            tyt=$tvar
+            continue
+        fi
+        if [ $tvar = "-m" ] ; then
+            i=$(( i+1 ))
+            tt=$(echo "\$$i" )
+            tvar=$(eval echo "$tt")
+            message=$(echo "$message\n$tvar")
+            height=$(( height+2 ))
+            continue
+        fi
+        if [ $tvar = "-h" ] ; then
+            i=$(( i+1 ))
+            tt=$(echo "\$$i" )
+            tvar=$(eval echo "$tt")
+            height=$tvar
+            continue
+        fi
+
+        if [ $tvar = "-w" ] ; then
+            i=$(( i+1 ))
+            tt=$(echo "\$$i" )
+            tvar=$(eval echo "$tt")
+            width=$tvar
+            continue
+        fi
+        if [ $tvar = "-ln" ] ; then
+            i=$(( i+1 ))
+            tt=$(echo "\$$i" )
+            tvar=$(eval echo "$tt")
+            nline=$tvar
+            continue
+        fi
+    done
+
+    INP_VAR=$(whiptail --radiolist "$message" --title "$tyt" $height $width $nline "${menux[@]}" 3>&1 1>&2 2>&3)
+
+    EX_STAT=$?
+
+}
