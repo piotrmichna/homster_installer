07885bb51cc823cfcedc39925968d12850320658
Piotr Michna <pm@piotrmichna.pl>
2020-06-21 17:15:06 +0200

    komentarze do pliku l_radiolist.sh

--- l_radiolist.sh
+++ l_radiolist.sh
@@ -6,13 +6,28 @@
 #   email: pm@piotrmichna.pl
 #--
 #   wywolanei funkcji:
-#       radiolist_var -h 20 -w 60 -ln 4 -m "Podaj hasło." -t "Hasło"
+#       radiolist_var -m "Wybierz paramet do zmiany:" -t " | Konfigurator usługi | "
 #       -h   wysokość okna  [liczba]
 #       -w   szerokość okna  [liczba]
 #       -ln  liczba wierszy [liczba]
 #       -t  "Tytuł okna"
 #       -m  "tekst z informacja"
 #--
+#   wynik wykonania
+#       case $INP_VAR in
+#       "1)")
+#           echo "Tytuł usługi"
+#           ;;
+#       "2)")
+#           echo "Nazwa usługi"
+#           ;;
+#       "3)")
+#           echo "Nazwa użytkownika"
+#           ;;
+#       "4)")
+#           EX=0
+#           ;;
+#       esac
 
 INP_VAR=0
 EX_STAT=0
