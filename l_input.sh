7942749d1fc6eccb05161b223ef5ff1299eae428
Piotr Michna <pm@piotrmichna.pl>
2020-06-21 15:47:45 +0200

    instrukcji walidacji parametrow przekazywanych do funkjci input_var()

--- l_input.sh
+++ l_input.sh
@@ -10,6 +10,14 @@ function input_var(){
     local message="Komnetarz do zawartosci"
     local valu=""
     local width=78
+    echo "ilosc parametrow ${#@}"
+    for (( i=1 ; i<=${#@} ; i++ )) ; do
+        tt=$(echo "\$$i" )
+        tvar=$(eval echo "$tt")
+        if [ $tvar = "-p" ] ; then
+            typ="p"
+            continue
+        fi
 
     if [ $typ = "p" ] ; then
         INP_VAR=$(whiptail --passwordbox "$message" 8 $width "$valu" --title "$tyt" 3>&1 1>&2 2>&3)
@@ -18,3 +26,9 @@ function input_var(){
     fi
     EX_STAT=$?
 }
+
+   if [ $EX_STAT = 0 ]; then
+       echo "Wprowadzono: " $INP_VAR " i wcisnieto OK"
+   else
+       echo "Anulowano wprowadzanie"
+   fi
