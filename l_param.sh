564d6f305368dce1ee0a75d2156300b0dc2af5e5
Piotr Michna <pm@piotrmichna.pl>
2020-06-22 22:52:52 +0200

    wstawienie parametrow z wszystkich bibliotek

--- l_param.sh
+++ l_param.sh
@@ -4,3 +4,20 @@
 #   lista parametrow instalatora
 #   autor: Piotr Michna
 #   email: pm@piotrmichna.pl
+
+CUR_DATE=$(date +"%F") #data
+USR="piot"
+SERVICE_DESCRIPTION="HOMSTER - system sterowania"
+SERVICE_NAME="homster.service"
+SERVICE_DIR="homster_dev"
+HOME_DIR="/home/${USR}"
+LOG_FILENAME="log"
+
+LOG_FILE="${LOG_FILENAME}_${CUR_DATE}.log"
+SRCDO="${HOME_DIR}/${SERVICE_DIR}"
+
+HTML_ROOT="/var/www/html"
+HTML_DOC="."
+MYSQL_USER="pituEl"
+MySQL_PASS="hi24biscus"
+MYSQL_ROOT_PASS="hi24biscus"
