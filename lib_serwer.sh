b0c7d1c80b44775d302ec4abba35aefdcded2ba8
Piotr Michna <pm@piotrmichna.pl>
2020-06-22 21:19:13 +0200

    definicja funkcji serwer_instal()

--- lib_serwer.sh
+++ lib_serwer.sh
@@ -8,3 +8,27 @@
 source lib_install.sh
 source l_checklist.sh
 
+function serwer_install(){
+    local menux[0]="nginx"
+    local menux[1]="Serwer www."
+    local menux[2]="on"
+    local menux[3]="php"
+    local menux[4]="Interperter skryptów PHP."
+    local menux[5]="on"
+    local menux[6]="mariadb"
+    local menux[7]="Serwer baz danych MySQL. "
+    local menux[8]="on"
+    local menux[9]="phpmyadmin"
+    local menux[10]="Interfejs www do zarządzania bazami MySQL. "
+    local menux[11]="on"
+    checklist_var -m "Wybierz zadanie do wykonania::" -t " | Instalator oprogramowania narzędziowego | "
+    local n=0
+    for i in ${INP_VAR[@]} ; do
+        local RE[$n]=$( echo "$i" | tr -d \" )
+        install_prog ${RE[$n]}
+        #echo "${RE[$n]}"
+        n=$(( n+1 ))
+    done
+    clear
+}
+#serwer_install
