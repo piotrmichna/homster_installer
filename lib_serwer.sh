fc0531189ac26ad75be561813e120cd7566fd32e
Piotr Michna <pm@piotrmichna.pl>
2020-06-22 21:17:09 +0200

    utworzenie pliku biblioteki lib_serwer.sh

---
+++ lib_serwer.sh
@@ -0,0 +1,10 @@
+# /bin/bash
+#   lib_serwer.sh
+
+#   instalator i konfigurator systemu HOMSTER
+#   autor: Piotr Michna
+#   email: pm@piotrmichna.pl
+
+source lib_install.sh
+source l_checklist.sh
+
