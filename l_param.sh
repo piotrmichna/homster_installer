9f006e13ac3621a09b4407fbfaf79f5f4f277758
Piotr Michna <pm@piotrmichna.pl>
2020-06-23 00:04:59 +0200

    definicja tablicy uslug

--- l_param.sh
+++ l_param.sh
@@ -21,3 +21,23 @@ HTML_DOC="."
 MYSQL_USER="pituEl"
 MySQL_PASS="hi24biscus"
 MYSQL_ROOT_PASS="hi24biscus"
+
+N_SERVICE_NUM=2
+N_USR[0]="piot"
+N_SERVICE_DESCRIPTION[0]="HOMSTER - system sterowania"
+N_SERVICE_NAME[0]="homster.service"
+N_SERVICE_DIR[0]="homster_dev"
+
+N_HTML_DOC[0]="."
+N_MYSQL_USER[0]="pituEl"
+N_MySQL_PASS[0]="hi24biscus"
+
+N_SERVICE_NUM=2
+N_USR[1]="pi"
+N_SERVICE_DESCRIPTION[1]="JASPP - system sterowania Garowni"
+N_SERVICE_NAME[1]="jaspp.service"
+N_SERVICE_DIR[1]="jaspp_dev"
+
+N_HTML_DOC[1]="."
+N_MYSQL_USER[1]="pituEl"
+N_MySQL_PASS[1]="hi24biscus"
