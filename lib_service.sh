23a868c0b14666b9da3c168e9973ccfe25d69b63
Piotr Michna <pm@piotrmichna.pl>
2020-06-23 00:06:37 +0200

    definicja funkcji service_list()

--- lib_service.sh
+++ lib_service.sh
@@ -9,6 +9,34 @@ source l_menulist.sh
 source l_input.sh
 source l_param.sh
 
+function service_list(){
+    local EX=1
+    while [ $EX -eq 1 ] ; do
+        local n=0
+        for (( i=0 ; i<N_SERVICE_NUM ; i++ )) ; do
+            local menux[$n]="$i"
+            n=$(( n+1 ))
+            local menux[$n]=" ${N_SERVICE_DESCRIPTION[$i]}. "
+            n=$(( n+1 ))
+        done
+        local menux[$n]="Koniec"
+        n=$(( n+1 ))
+        local menux[$n]=" Powrót do konfiguratora usługi. "
+        menulist_var -m "Wybierz usługę systemową do zainstalowania" -t " | Lista usług systemowych | " -w 100
+        if [ "$INP_VAR" = "Koniec" ] ; then
+            EX=0
+        else
+            USR=${N_USR[$INP_VAR]}
+            SERVICE_DESCRIPTION=${N_SERVICE_DESCRIPTION[$INP_VAR]}
+            SERVICE_NAME=${N_SERVICE_NAME[$INP_VAR]}
+            SERVICE_DIR=${N_SERVICE_DIR[$INP_VAR]}
+            HTML_DOC=${N_HTML_DOC[$INP_VAR]}
+            MYSQL_USER=${N_MYSQL_USER[$INP_VAR]}
+            MySQL_PASS=${N_MySQL_PASS[$INP_VAR]}
+            EX=0
+        fi
+    done
+}
 
 function service_main(){
     local EX=1
