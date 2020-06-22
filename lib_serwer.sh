8b1d21b7a7305c3cc8b53923b4f1a33ae6f11cc4
Piotr Michna <pm@piotrmichna.pl>
2020-06-22 22:28:10 +0200

    definicja funkcji edycji parametrow serwer_param()

--- lib_serwer.sh
+++ lib_serwer.sh
@@ -14,7 +14,62 @@ HTML_ROOT="/var/www/html"
 HTML_DOC="."
 MYSQL_USER="pituEl"
 MySQL_PASS="hi24biscus"
-MYSQL_ROOT_PAS="hi24biscus"
+MYSQL_ROOT_PASS="hi24biscus"
+
+function serwer_param(){
+    local EX=1
+    while [ $EX -eq 1 ] ; do
+        local menux[0]=" HTML_ROOT "
+        local menux[1]=" Katalog główny serwera www [$HTML_ROOT]. "
+        local menux[2]=" HTML_DOC "
+        local menux[3]=" Katalog usługi www [$HTML_DOC]. "
+        local menux[4]=" MYSQL_USER "
+        local menux[5]=" Użytkownik usługi MySQL [$MYSQL_USER]. "
+        local menux[6]=" MySQL_PASS "
+        local menux[7]=" Hasło użytkownika usługi MySQL [$MySQL_PASS]. "
+        local menux[8]=" MYSQL_ROOT_PASS "
+        local menux[9]=" Hasło root serwera MySQL [$MYSQL_ROOT_PASS]. "
+        local menux[10]=" Koniec "
+        local menux[11]=" Powrót do poprzedniego menu. "
+        menulist_var -m "Wybierz paramet do zmiany:" -t " | Konfigurator usługi | "
+
+        case $INP_VAR in
+        " HTML_ROOT ")
+            input_var -t " | HTML_ROOT | " -w 60 -m "Podaj ścieżkę do katalogu głównego dla serwera www." -v "$HTML_ROOT"
+            if [ $EX_STAT = 0 ]; then
+                HTML_ROOT=$INP_VAR
+            fi
+            ;;
+        " HTML_DOC ")
+            input_var -t " | HTML_DOC | " -w 60 -m "Podaj katalog usługi www. \'.\' oznacza katalog główny serwera www." -v "$HTML_DOC"
+            if [ $EX_STAT = 0 ]; then
+                HTML_DOC=$INP_VAR
+            fi
+            ;;
+        " MYSQL_USER ")
+            input_var -t " | MYSQL_USER | " -w 60 -m "Podaj nazwę użytkownik MySQL dla usługi www." -v "$MYSQL_USER"
+            if [ $EX_STAT = 0 ]; then
+                MYSQL_USER=$INP_VAR
+            fi
+            ;;
+        " MySQL_PASS ")
+            input_var -t " | MySQL_PASS | " -w 60 -m "Podaj hasło użytkownik MySQL dla usługi www." -v "$MySQL_PASS"
+            if [ $EX_STAT = 0 ]; then
+                MySQL_PASS=$INP_VAR
+            fi
+            ;;
+        " MYSQL_ROOT_PASS ")
+            input_var -t " | MYSQL_ROOT_PASS | " -w 60 -m "Podaj hasło użytkownik root dla MySQL." -v "$MYSQL_ROOT_PASS"
+            if [ $EX_STAT = 0 ]; then
+                MYSQL_ROOT_PASS=$INP_VAR
+            fi
+            ;;
+        " Koniec ")
+            EX=0
+            ;;
+       esac
+    done
+}
 
 function php_install(){
     sudo apt-get install php php-cli php-fpm php-mbstring php-gettext -y &> /dev/null
@@ -68,7 +123,7 @@ function serwer_install(){
             continue
         fi
         if [ ${RE[$n]} = "parametry" ] ; then
-            serwer_conf
+            serwer_param
             n=$(( n+1 ))
             continue
         else
