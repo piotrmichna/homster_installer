af41cec12b222bd2867cfc9e204568e60ec0a9b2
Piotr Michna <pm@piotrmichna.pl>
2020-06-22 21:36:50 +0200

    modyfikacja funkcji serwer_install()

--- lib_serwer.sh
+++ lib_serwer.sh
@@ -9,32 +9,50 @@ source lib_install.sh
 source l_checklist.sh
 
 function php_install(){
-    sudo apt-get install php php-cli php-fpm -y &> /dev/null
+    sudo apt-get install php php-cli php-fpm php-mbstring php-gettext -y &> /dev/null
 }
 
 function maridb_install(){
     sudo apt-get install mariadb-server-10.0 mariadb-client-10.0 php-mysql -y &> /dev/null
 }
 function serwer_install(){
-    local menux[0]="nginx"
-    local menux[1]="Serwer www."
+    local menux[0]="Nginx"
+    local menux[1]=" Serwer www. "
     local menux[2]="on"
-    local menux[3]="php"
-    local menux[4]="Interperter skryptów PHP."
+    local menux[3]="PHP"
+    local menux[4]=" Interperter skryptów PHP. "
     local menux[5]="on"
-    local menux[6]="mariadb"
-    local menux[7]="Serwer baz danych MySQL. "
+    local menux[6]="MariaDB"
+    local menux[7]=" Serwer baz danych MySQL. "
     local menux[8]="on"
     local menux[9]="phpmyadmin"
-    local menux[10]="Interfejs www do zarządzania bazami MySQL. "
+    local menux[10]=" Interfejs www do zarządzania bazami MySQL. "
     local menux[11]="on"
     checklist_var -m "Wybierz zadanie do wykonania::" -t " | Instalator oprogramowania narzędziowego | "
     local n=0
     for i in ${INP_VAR[@]} ; do
         local RE[$n]=$( echo "$i" | tr -d \" )
-        install_prog ${RE[$n]}
+        if [ ${RE[$n]} = "Nginx" ] ; then
+            install_prog "nginx"
+            n=$(( n+1 ))
+            continue
+        fi
+        if [ ${RE[$n]} = "PHP" ] ; then
+            php_install
+            n=$(( n+1 ))
+            continue
+        fi
+        if [ ${RE[$n]} = "MariaDB" ] ; then
+            mariadb_install
+            n=$(( n+1 ))
+            continue
+        else
+            install_prog ${RE[$n]}
+            n=$(( n+1 ))
+        fi
+
         #echo "${RE[$n]}"
-        n=$(( n+1 ))
+
     done
     clear
 }
