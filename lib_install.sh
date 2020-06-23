b03c4bc15a3aafdf9008e3e9e9979ae6f025b650
Piotr Michna <pm@piotrmichna.pl>
2020-06-23 19:47:14 +0200

    poprawienie wyswietlanych komunikatow

--- lib_install.sh
+++ lib_install.sh
@@ -11,12 +11,12 @@ function install_prog(){
     local mess=""
     dpkg -s $1 &> /dev/null
     if [ $? -eq 0 ] ; then
-        echo "---> $1 JEST JUŻ ZAINSTALOWANY" >> ${HOME_DIR}/${LOG_FILENAME}_$currentDate.log &> /dev/null
+        echo "---> $1 JEST JUŻ ZAINSTALOWANY" |& tee -a ${HOME_DIR}/${LOG_FILENAME}_$currentDate.log &> /dev/null
         TERM=ansi whiptail --title "- $1 -" --infobox "$1 był już zainstalowany" 8 70
         clear
     else
         echo "--->INSTALL" >> ${HOME_DIR}/${LOG_FILENAME}_$currentDate.log &> /dev/null
-        TERM=ansi whiptail --title "INSTALATOR - $1" --infobox "Oprogramowanie $1 zostało pomyślnie zainstalowane." 8 70
+        TERM=ansi whiptail --title "INSTALATOR - $1" --infobox "Trwa instalacja oprogramowania $1." 8 70
         sudo apt-get install -y $1 |& tee -a ${HOME_DIR}/${LOG_FILENAME}_$currentDate.log &> /dev/null
         clear
     fi
