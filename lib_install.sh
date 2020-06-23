38ff152657b0a88171de22b3b3ccc8619c371d68
Piotr Michna <pm@piotrmichna.pl>
2020-06-23 03:39:45 +0200

    modyfikacja logow

--- lib_install.sh
+++ lib_install.sh
@@ -11,10 +11,12 @@ function install_prog(){
     local mess=""
     dpkg -s $1 &> /dev/null
     if [ $? -eq 0 ] ; then
-        echo -e "${GREEN}---> $1 jest już zainstalowane $NC" |& tee -a ${HOME_DIR}/${LOG_FILENAME}_$currentDate.log
+        echo -e "${GREEN}---> $1 jest już zainstalowane $NC"
+        echo "---> $1 JEST JUŻ ZAINSTALOWANY" > ${HOME_DIR}/${LOG_FILENAME}_$currentDate.log
         TERM=ansi whiptail --title "- $1 -" --infobox "$1 był już zainstalowany" 8 70
     else
-        echo -e "${GREEN}--->INSTALL $1 $NC" |& tee -a ${HOME_DIR}/${LOG_FILENAME}_$currentDate.log
+        echo -e "${GREEN}--->INSTALL $1 $NC"
+        echo "--->INSTALL" > ${HOME_DIR}/${LOG_FILENAME}_$currentDate.log
         sudo apt-get install -y $1 |& tee -a ${HOME_DIR}/${LOG_FILENAME}_$currentDate.log &> /dev/null
         TERM=ansi whiptail --title "INSTALATOR - $1" --infobox "Oprogramowanie $1 zostało pomyślnie zainstalowane." 8 70
     fi
